285fe8dc387696d7bb72fa674179f7ffd0eb05f9
abdurresitdemir <abdurresitdemir@gmail.com>
2025-07-10 17:30:08 +0300

    projenın api service kısmı model kısmı olusturuldu ve global ozeleştirilmemis yaberler ekranı yapıldı

---
+++ lib/core/utilis/constants.dart
@@ -0,0 +1,7 @@
+final String imageanahtar =
+    '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';
+final String url_bbc =
+    'http://feeds.bbci.co.uk/news/technology/rss.xml';
+
+final String _globalUrl = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
+
