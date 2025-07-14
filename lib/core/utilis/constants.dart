8b0058737eea3ee57e95dfa1e35f0db0d59bdff7
abdurresitdemir <abdurresitdemir@gmail.com>
2025-07-14 13:12:08 +0300

    kişiye ozel haberler ve bircok api servise bitti

--- lib/core/utilis/constants.dart
+++ lib/core/utilis/constants.dart
@@ -3,5 +3,4 @@ final String imageanahtar =
 final String url_bbc =
     'http://feeds.bbci.co.uk/news/technology/rss.xml';
 
-final String _globalUrl = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
-
+const String kGlobalNewsUrl = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
